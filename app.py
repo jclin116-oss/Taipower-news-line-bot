a51ecb36147c0cc48e29ff75b84feec0997b0b07
jclin116-oss <jclin116@gmail.com>
2026-08-19 08:31:06 +0800

    app.py

--- app.py
+++ app.py
@@ -13,9 +13,10 @@ LINE_TOKEN = os.environ.get(
     'ZoCExUoUpUQ+3A9LhOGq5doeaylobJFgSO/2Tkwyz72qGs+gHiJsWZuMQmTXHidWC2I/WROppq36PysQxBT2Z8suEL+ZyVVEod8tzPYkN4wAokHiqXMoy9z4ZVu0OMDYgjsdjNYq3ZP+w2GCcXf/GwdB04t89/1O/w1cDnyilFU=',
 )
 
-TARGET_IDS = [
-        'U6ac6a7a58e085194ac436f346d803aad',  # 課長個人 ID
-]
+TARGET_IDS =  [
+    'C3b3a18e6bddc2ee3ac4c9347d67c4031',  # 服務所群組
+   ]
+
 
 DEFAULT_KEYWORDS = '基隆 台電'
 SEARCH_HOURS = 24
